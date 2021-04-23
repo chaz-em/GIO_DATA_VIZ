feccdb1ac0e0168e19e7c467a06e9606e64a1742
chaz-em <dcharlesmanou@gmail.com>
2021-04-23 11:46:12 +0000

    fix affichage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
 $( document ).ready(function() {
-    document.body.innerHTML = '<div id="zoneArea" style="width: 500px; height: 500px; background-color: red"></div>';
+    document.body.innerHTML = '<div id="globe" style="width: 500px; height: 500px;></div>';
 
-  
         // Get the container to hold the IO globe
-        var container = document.getElementById( "zoneArea" );
+        var container = document.getElementById( "globe" );
 
         // Create Gio.controller
-        var controller = new GIO.Controller( container );
+        var globe = new GIO.Controller( container );
 
         // Add data
-        controller.addData( data );
-
+        globe.addData( data );
+        //globe.setStyle("lemonGate");
+        globe.setSurfaceColor("#FF4580");
+        //globe.setSelectedColor("ff8000");
+        //globe.setInitCountry("US");
+        //globe.lightenMentioned(false);
         // Initialize and render the globe
-        controller.init();
+        globe.init();
 });
